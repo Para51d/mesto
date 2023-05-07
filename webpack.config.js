b37ba3b0ea75363af8fcdf72b68938b255eb83e6
Aleksandr Sakulin <great.sakulin@yandex.ru>
2023-05-08 01:50:42 +0300

    Девятый спринт правки1

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     entry: { main: './src/pages/index.js' },
 
-    devtool: 'inline-source-map',
+    //devtool: 'inline-source-map',
 
     output: {
         path: path.resolve(__dirname, 'dist'),
